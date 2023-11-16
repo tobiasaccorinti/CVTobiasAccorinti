4e6c5b1f255bf4334709e20657aaa3d54ba895c7
tobiasaccorinti <142542084+tobiasaccorinti@users.noreply.github.com>
2023-11-16 14:34:19 -0300

    Add files via upload

--- java.js/java.js
+++ java.js/java.js
@@ -1,7 +1,18 @@
+let num1;
+let num2;
+let contador = 1;
 
-var TextoHeader = document.getElementsByClassName('TextoHeader')
-TextoHeader.addEventListener("click" , clicked)
+num1 = Number(prompt('Escriba un numero entre 1 y 100'));
+num2 = Math.round(Math.random() * 100);
 
-function clicked(){
-    alert('Has hecho click')
+while (num1 !== num2) {
+    if (num1 < num2) {
+        alert('El numero pensado es más ALTO');
+    } else {
+        alert('El numero pensado es más BAJO');
+    }
+    num1 = Number(prompt('Escriba otro numero'));
+    contador++;
 }
+
+alert('¡Felicidades! El numero pensado era ' + num2 + '.\n Lo resolviste en ' + contador + ' intentos.');
